f194fb2cc8006fd9f73fb03854e232de0e8abc35
Paula Ricci <ricci.paula.e@gmail.com>
2022-10-30 02:11:41 -0300

    02113010

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ function App() {
           <NavBar/>
           <Routes>
             <Route path='/' element={<ItemListContainer/>} />
+            <Route path='/ProyectoFinal-Ricci/' element={<ItemListContainer/>} />
             <Route path='/category/:id' element={<ItemListContainer/>} />
             <Route path='/item/:id' element={<ItemDetailContainer/>} />
             <Route path='/cart' element={<Cart/>} />
             <Route path='/checkout' element={<Checkout/>} />
           </Routes>
         </BrowserRouter>
-        <ItemListContainer/>
       </div>
     </CartContextProvider>
     <Footer/>
